953dcf6a10560ee3fd0258c8abed4ce2dbdfc003
Md Raza <mdraza8397@gmail.com>
2025-11-26 23:12:15 +0530

    26112025 minor changes

--- coding.py
+++ coding.py
@@ -1,2 +1,6 @@
-name = input("Enter your name: ")
-print("My name is: ", name)
+# name = input("Enter your name: ")
+x = 500
+y = 600
+z = x + y
+print("The sum of x and y is: ", z)
+# print("My name is: ", name)
